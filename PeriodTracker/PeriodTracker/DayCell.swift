17004d9e49c77f87636f43601e0d68ff1a8e9d03
Paola Castro <paolacastropaolacastro@gmail.com>
2020-03-17 18:30:39 -0700

    mininum collection view

---
+++ PeriodTracker/PeriodTracker/DayCell.swift
@@ -0,0 +1,20 @@
+//
+//  DayCell.swift
+//  PeriodTracker
+//
+//  Created by Paola Castro on 3/17/20.
+//  Copyright © 2020 Paola Castro. All rights reserved.
+//
+
+import UIKit
+
+class DayCell: UICollectionViewCell {
+    @IBOutlet weak var background: UIView!
+    @IBOutlet weak var dayLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ PeriodTracker/PeriodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PeriodTracker
+//
+//  Created by Paola Castro on 3/16/20.
+//  Copyright © 2020 Paola Castro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
